e4b699649e0942cb4266cfe36d6f3055486c4fba
m3yevn <mmm.myintmyat@gmail.com>
2023-05-21 23:25:31 +0800

    init: repo

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="welcome">
+    <h1>
+      <a href="https://github.com/m3yevn">@m3yevn</a> is cooking something!
+    </h1>
+  </div>
+</template>
+
+<style>
+.welcome {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+body {
+  margin: 0;
+}
+</style>
