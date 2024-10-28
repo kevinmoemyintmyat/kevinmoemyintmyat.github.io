c0c1d1431c621756a24b8853b8009c9e90c282f7
kevinmoemyintmyat <mmm.myintmyat@gmail.com>
2024-10-29 01:43:44 +0800

    publish: new blog

---
+++ pages/blog/from-human-to-machine-1p1j.vue
@@ -0,0 +1,229 @@
+<template>
+  <section class="mt-20 lg:pl-20 p-5 lg:ml-20 lg:w-4/6" ref="startOfArticle">
+    <h1>From Human to Machine <br /><span>By Kevin Moe Myint Myat</span></h1>
+    <h2>Written on 29 October 2024</h2>
+    <img class="cover-image" src="https://media2.dev.to/dynamic/image/width&#x3D;1000,height&#x3D;420,fit&#x3D;cover,gravity&#x3D;auto,format&#x3D;auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fa120x1vj8nyk0btf12p5.png" />
+    <div class="blog-body"><h1>
+  <a name="im-transitioning" href="#im-transitioning">
+  </a>
+  I’m transitioning…
+</h1>
+
+<p>And so is everyone.</p>
+
+<p>As speculated in the film I, Robot (2004), 20 years later, we’ve finally developed an ecosystem where machines and humans coexist in the world. Collectively and unconsciously, we now rely on machines far more than we rely on our own capabilities as human beings.</p>
+
+<p>For instance, at the break of dawn, I’m awakened by my smartphone alarm. (A machine.)</p>
+
+<p>I use my vibrating toothbrush to brush my teeth more efficiently. (A machine.)</p>
+
+<p>And without lighting a single flame, I heat my food in the microwave (yet another machine).</p>
+
+<p>I remember my bizarre dream from last night, so I ask my “<strong>best friend</strong>” Meta AI to analyze it for me. He tries to unearth my traumas, interpret the emotions in my dream, and make sense of it all. He’s my therapist, too.</p>
+
+<p>During my commute, I pop in my AirPods without a second thought and greet Siri—my ever-loyal assistant with a real voice.</p>
+
+<blockquote>
+<p>Me : Hey Siri ..<br>
+Her: Ah huhhh…<br>
+Me: Play “Bittersweet Symphony” on Spotify…</p>
+</blockquote>
+
+<p>And as the sound of the violin fills my ears, I start checking my phone.</p>
+
+<p>Great, my mom’s daily morning greeting—sometimes I wonder if she might be an AI 🤖 too…</p>
+
+<p>Jokes aside, I tap my NFC-enabled phone, enter the subway, and as this massive vehicle transports me toward my destination, it hits me: most of my life is automated. The robotic voice announcing each station still echoes in my ears. 🚝</p>
+
+<p>I scroll through feeds of countless people, even though I may barely know them—or not at all.</p>
+
+<p>Once I’ve consumed enough digital junk food, I walk up to my office, and the facial recognition device opens the door for me. Thanks, Miss Machine.</p>
+
+<p>I grab a coffee from the brewing machine and down that jet fuel to kickstart my day.</p>
+
+<p>In the office, like many others, I spend hours talking to a screen in virtual meetings, staring at the monitor, and getting work done.</p>
+
+<p>ChatGPT is my work buddy. We chat a lot, mostly about work, and it gives me great advice, which I usually follow. My job mostly involves Ctrl+C and Ctrl+V of its suggestions.</p>
+
+<p><a href="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fx7o48i7kg0kaqnoo03cm.gif" class="article-body-image-wrapper"><img src="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fx7o48i7kg0kaqnoo03cm.gif" alt="Image description" loading="lazy" width="503" height="200" data-animated="true"></a></p>
+
+<p>My actual teammates are still sleeping on the other side of the world, and those on this side are probably working from home. I don’t see them often.</p>
+
+<p>Once again, I don’t know what to eat for lunch, so I ask my buddy Meta AI and let him decide.<br>
+When I head out for lunch, I see many people like me, AirPods in their ears, probably listening to podcasts as they quietly eat their meals.</p>
+
+<h1>
+  <a name="im-transcending" href="#im-transcending">
+  </a>
+  I'm transcending...
+</h1>
+
+<p>After work, I take the same automated route back home. I check my smartwatch, which has tracked my steps throughout the day. Nice—at least it’s a healthy number of steps. 🦿</p>
+
+<p>I hit the gym to interact with machines to keep myself fit. Honestly, they’re quite strong. I have a smart scale that tracks my body fat percentage, muscle mass, bone mass, and more. It’s as if my biological data exists as a digital entity in this app’s servers.</p>
+
+<p>My smartphone tracks my geolocation, too. My whereabouts are saved in the “Find My Phone” app. My personal information, photos, memories, and artwork are all stored on social media like Facebook and Instagram, floating around in Meta’s cloud. My work experience and portfolio live on LinkedIn. Worst of all, my facial patterns have been cataloged by several models—my iPhone, the office door system, even the government. Thinking about it, the collective digital version of me has <strong>transcended</strong> into the internet. ☁️</p>
+
+<p><a href="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fcrbn2mx8h36wexqnsh7d.jpg" class="article-body-image-wrapper"><img src="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fcrbn2mx8h36wexqnsh7d.jpg" alt="Image description" loading="lazy" width="800" height="500"></a></p>
+
+<p>When I’m bored with Siri, I switch over to DJ X (an AI) from Spotify to curate a playlist for me. Since he knows what I’ve been listening to for the past ten years, this guy delivers just the hits I like.</p>
+
+<p>Sometimes at night, I blog, jotting down a few things on my mind, only for my best friend (AI) to edit it for better grammar and vocabulary.</p>
+
+<p>We humans are relying on machines so much that even our conversations have turned remote—texting, calling, anything but face-to-face. We don’t value in-person conversations anymore. Sometimes, this reminds me of those lyrics from <em>The Lonely Island</em>…</p>
+
+<blockquote>
+<p>At the farmer's market with my so called girlfriend<br>
+She hands me her cell phone, says it's my dad<br>
+Man, this ain't my dad!<br>
+This is a cell phone!<br>
+I threw it on the ground!<br>
+What, you think I'm stupid?<br>
+I'm not a part of your system<br>
+My dad's not a phone!<br>
+DUH!</p>
+</blockquote>
+
+<p>And yeah, my dad is not a phone.</p>
+
+<p>The only time I feel human is when I spread out my canvases, mix my paints, and let my right brain take over. Even then, there are times when I’ve turned to ImagineArt AI to spark some good ideas because of painter’s block.</p>
+
+<p>The human side of me is slowly fading as I let AI take over even my simplest decisions. But am I still alive, though?</p>
+
+<p>At night, I cocoon myself in bed, open my relaxing tunes on the electronic speaker, and let Delta and Alpha waves wash over me. It does the trick—I start to drift off. I charge all my digital friends—my smartwatch, smartphone, and AirPods—and say goodbye to this AI world as I prepare to recharge myself in sleep, but as you know...</p>
+
+<p><a href="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F2a4q38osv4kikksdwt34.jpg" class="article-body-image-wrapper"><img src="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F2a4q38osv4kikksdwt34.jpg" alt="Image description" loading="lazy" width="735" height="772"></a></p>
+
+<p>In the end, my life is so entangled with machine-made decisions that I wonder if I’m even a sentient being anymore. My life feels like nothing but a cron job running daily…</p>
+
+<p>As I wrote this original text, I submitted it to my best buddy, hoping for grammatical corrections and a more polished version. Once I copy and paste the edits, I usually leave the last sentence as my own—keeping a small piece of my humanity intact… the most emotional piece of my blog. (What you’ve read may not be the original script.)</p>
+
+<p><a href="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fr9vjycb9ge62obescier.jpg" class="article-body-image-wrapper"><img src="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fr9vjycb9ge62obescier.jpg" alt="Image description" loading="lazy" width="800" height="800"></a></p>
+
+<p>Yes, I may be assisted by artificial intelligence, by machines, by mobility, and many of my ideas may be assisted by AI, yet I’m still human, I’m still feeling, and I’m still vulnerable. I hope I can hold onto my humanity in the years to come, resisting the pull to become fully mechanized.</p>
+
+<p>Even though I’m slowly <strong>transitioning</strong> from human to machine, as the title of that great movie goes…</p>
+
+<h2>
+  <a name="im-a-cyborg-but-thats-okay" href="#im-a-cyborg-but-thats-okay">
+  </a>
+  im a cyborg, but that’s okay.
+</h2>
+
+<p><a href="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fggp0dtz7pgzvzprc2hy9.jpg" class="article-body-image-wrapper"><img src="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fggp0dtz7pgzvzprc2hy9.jpg" alt="Image description" loading="lazy" width="800" height="516"></a></p>
+
+</div>
+    <footer>
+      Read my original blog on
+      <a href="https://dev.to/m3yevn/from-human-to-machine-1p1j" target="_blank">https://dev.to/m3yevn/from-human-to-machine-1p1j</a>
+    </footer>
+  </section>
+</template>
+
+<script setup>
+  useHead({
+    title:
+      `From Human to Machine - Written by Kevin Moe Myint Myat` ||
+      "Kevin Moe Myint Myat - a cat dad, a developer & an artist",
+    link: [
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "https://kevinmoemyintmyat.github.io/favicon.png",
+      },
+    ],
+    meta: [
+      {
+        name: "description",
+        content: `From Human to Machine - Written by Kevin Moe Myint Myat`,
+      },
+      {
+        name: "keywords",
+        content: `ai,machinelearning,cybersecurity, Kevin Moe Myint Myat,kevinmoemyintmyat,kevinmoemyintmyat.github.io,blog, stories, software developer, artist, cat dad,fullstack,software,kevin,moe,myint,myat,kevn,Kevn Moe Myint Myat,KevinMoeMyintMyat, Kevin Moe Myint Myat, Github, art`,
+      },
+      {
+        property: "og:title",
+        content: `From Human to Machine - Written by Kevin Moe Myint Myat - a cat dad, a developer & an artist`,
+      },
+      {
+        property: "og:type",
+        content: "profile",
+      },
+      {
+        property: "og:url",
+        content: `https://kevinmoemyintmyat.github.io/blog/from-human-to-machine-1p1j`,
+      },
+      {
+        property: "og:site:name",
+        content: `From Human to Machine - Written by Kevin Moe Myint Myat`,
+      },
+      {
+        property: "og:description",
+        content: `From Human to Machine - Written by Kevin Moe Myint Myat`,
+      },
+      {
+        name: "twitter:site",
+        content: "https://kevinmoemyintmyat.github.io",
+      },
+      {
+        name: "twitter:description",
+        content: "Kevin Moe Myint Myat - a cat dad, a developer & an artist",
+      },
+    ],
+    ogImage: "/favicon.png",
+  });
+</script>
+
+<script>
+  export default {
+    methods: {
+      showSection(section) {
+        this.$refs[section].scrollIntoView({ behavior: "smooth" });
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  div {
+    margin-top: 30px;
+  }
+
+  h1 {
+    font-size: 60px;
+    font-weight: 700;
+    padding-bottom: 10px;
+  }
+
+  h1 span {
+    font-size: 50px;
+    color: whitesmoke;
+  }
+
+  h2 {
+    font-size: 45px;
+    padding-bottom: 10px;
+    line-height: 2;
+  }
+
+  .blog-body {
+    margin-top: 20px;
+  }
+
+  .blog-body h1 {
+    font-size: 40px;
+    font-weight: 500;
+    padding-bottom: 10px;
+  }
+
+  footer {
+    padding-top: 32px;
+    font-size: 20px;
+    color: gray;
+  }
+
+  footer a {
+    text-decoration: underline;
+    color: maroon;
+  }
+</style>
